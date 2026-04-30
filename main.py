228e000aabea7ac6e811137bbe414b579fe2293f
akudohtudelft <akudohliang@tudelft.nl>
2026-04-30 20:14:29 +0200

    added constants

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
-    print("Hello from solarsailing!")
+    # constants
+    S = 12
+    We = 1361
+    Re = 1.495979 * 10**11
+    c - 3*10**8
+    m = 1
+    B = S*2*(We*Re**2)/c/m
 
+    # differential Equation
+    a = B*r**(-2)
 
 if __name__ == "__main__":
     main()
